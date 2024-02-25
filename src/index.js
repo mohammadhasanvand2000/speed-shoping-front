fe117bbbd3fdf627c4dd5408e7508ec85b5879bd
mohammadhasanvand200 <mhasanvand309@gmail.com>
2024-02-25 16:18:35 +0330

    ede

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 // index.js
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter} from 'react-router-dom';
 import App from './App';
 import { AuthProvider } from './components/auth/AuthContext';
 
 ReactDOM.render(
   
-   <Router >
+   <HashRouter >
       <App />
-   </Router>   
+   </RouterHashRouter>   
   ,
   
   document.getElementById('root')
